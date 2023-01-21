8380629a45685f12f757cb33eb469c560757e5ec
Tolotra <tolotor@yahoo.fr>
2023-01-21 21:36:24 +0300

    Env variables loading

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import http from "http";
+import path from "path";
+import dotenv from "dotenv";
 
 // Database setup
 import setupDatabase from "./db/setup";
 
 // Express REST API application
 import app from "./app";
+
+// Loading environment variables (only for local development)
+if (!process.env.NODE_ENV) dotenv.config({ path: path.join(__dirname, "../", ".env") });
 
 // HTTP server
 const server = http.createServer(app);
@@ -13,4 +18,6 @@ setupDatabase();
 
 // Listening to a port
 const PORT: number | string = process.env.PORT || 5000;
-server.listen(PORT, () => { console.log(`Server running on port ${PORT} ...`); });
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT} ...`);
+});
