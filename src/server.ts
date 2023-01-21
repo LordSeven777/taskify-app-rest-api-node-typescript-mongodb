224a20b86d59e7bc8a8bca9562c9e677873a5928
Tolotra <tolotor@yahoo.fr>
2023-01-21 11:17:39 +0300

    Database setup

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import http from "http";
 
+// Database setup
+import setupDatabase from "./db/setup";
+
 // Express REST API application
 import app from "./app";
 
 // HTTP server
 const server = http.createServer(app);
+
+setupDatabase();
 
 // Listening to a port
 const PORT: number | string = process.env.PORT || 5000;
