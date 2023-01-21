088e0b1a3d6aced9f3dbee35647266665c1a870b
Tolotra <tolotor@yahoo.fr>
2023-01-21 13:51:00 +0300

    Error handler middleware and index route

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,19 @@
 import express from "express";
 
+// The error handler
+import errorHandlerMiddleware from "./middlewares/errorHandler";
+
 // The express application
 const app = express();
 
+// Index
 app.get("/", (req, res) => {
-  res.json({ message: "Hello world" })
-})
+  res.send(`
+    <h1>This is the taskify app's rest api.</h1>
+    <p>If you want to access the resources, please point the routes available.</p>
+  `);
+});
+
+app.use(errorHandlerMiddleware);
 
 export default app;
