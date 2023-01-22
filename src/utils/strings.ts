93cd8e30c6e4ce034cf211e60088d3729308d2b9
Tolotra <tolotor@yahoo.fr>
2023-01-22 19:37:14 +0300

    Strings helper: First letter is uppercase checker

--- src/utils/strings.ts
+++ src/utils/strings.ts
@@ -1,3 +1,8 @@
 export function lowerFirstLetter(word: string): string {
   return word.charAt(0).toLowerCase().concat(word.substring(1));
 }
+
+export function firstLetterIsUpperCase(word: string): boolean {
+  const flCharCode = word.charCodeAt(0);
+  return flCharCode >= 97 && flCharCode <= 122;
+}
