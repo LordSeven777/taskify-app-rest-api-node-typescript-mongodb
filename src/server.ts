9220fbb4ecd0d8776769205c51808571d7b6f61f
Tolotra <tolotor@yahoo.fr>
2023-02-08 09:54:05 +0300

    dotenv configuration

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,10 @@
 import http from "http";
-import path from "path";
-import dotenv from "dotenv";
 
 // Database setup
 import setupDatabase from "@db/setup";
 
 // Express REST API application
 import app from "./app";
-
-// Loading environment variables (only for local development)
-if (!process.env.NODE_ENV) dotenv.config({ path: path.join(__dirname, "../", ".env") });
 
 // HTTP server
 const server = http.createServer(app);
